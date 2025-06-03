ecc1a27b690d2f42ebe40e839aedb0ab7da989bd
BranalEc <20240208@ricaldone.edu.sv>
2025-06-03 11:03:43 -0600

    Primer cambio

---
+++ src/main/java/BrandonOrellana_20240208/Main.java
@@ -0,0 +1,10 @@
+package BrandonOrellana_20240208;
+
+public class Main {
+    public static void main(String[] args) {
+        inventario demo = new inventario();
+
+        System.out.println("Sistema de inventario");
+        demo.StockInventario();
+    }
+}
---
+++ src/main/java/BrandonOrellana_20240208/inventario.java
@@ -0,0 +1,57 @@
+package BrandonOrellana_20240208;
+
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class inventario {
+
+    public void StockInventario() {
+        TreeMap<String, Integer> mapa = new TreeMap<>();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Seleccione una operacion");
+        System.out.println("1 -- Consultar");
+        System.out.println("2 -- Actualizar");
+        int opc = sc.nextInt();
+        switch (opc){
+            case 1:
+                System.out.println("Inventario" + mapa);
+                System.out.println("Stock");
+                break;
+            case 2:
+                System.out.println("Actualizar stock: ");
+                mapa.put("Papel",6);
+                mapa.put("PC",10);
+                mapa.put("Mouse",15);
+                mapa.put("Teclado",5);
+
+                System.out.println("Stock inicial " + mapa);
+
+                System.out.println("Cuántos prodcutos qiere actualizar?");
+                int cantidad = sc.nextInt();
+                sc.nextLine();
+
+                for (int i = 0; i < cantidad; i++) {
+                    System.out.print("Ingresa la clave para actualizar su valor: ");
+                    String clave = sc.nextLine();
+
+                    if (mapa.containsKey(clave)) {
+                        System.out.print("Ingresa el nuevo valor para la clave '" + clave + "': ");
+                        int nuevoValor = sc.nextInt();
+                        sc.nextLine();
+
+                        mapa.put(clave, nuevoValor);
+                        System.out.println("Valor actualizado: " + clave + " = " + nuevoValor);
+                    } else {
+                        System.out.println("La clave '" + clave + "' no existe en el mapa. No se actualizó.");
+                    }
+                }
+                    System.out.println("Stock actulizado: " + mapa);
+                    break;
+                default:
+                System.out.println("Entrada no valida");
+
+                break;
+        }
+    }
+}
